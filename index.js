39252cd394934c7574c25435e3d749e0c772fcd4
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-04-05 17:17:20 +0200

    commit

--- index.js
+++ index.js
@@ -34,3 +34,18 @@ document.getElementById("scheduleButton").addEventListener("click", function() {
         document.getElementById("contactDetails").value = "";
     }
 });
+
+
+function validateSyntax() {
+    let petInput = document.getElementById('petInput').value.trim();
+    let regex = /^pet_\d+[a-zA-Z]+$/;
+    let isValid = regex.test(petInput);
+    // Validation logic goes here
+    let resultElement = document.getElementById("result"); // Placeholder for validation result
+
+    if (isValid) {
+        resultElement.innerHTML = "Valid Syntax." + '<span style="color: green; font-size: 60px;">&#x25cf;</span>';
+    } else {
+        resultElement.innerHTML = "Invalid Syntax." + '<span style="color: red; font-size: 60px;">&#x25cf;</span>';
+    }
+}
